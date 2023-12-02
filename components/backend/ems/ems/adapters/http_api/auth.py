451ea24cd610da7030bca60e00bff83b11a725be
Evgeny <mangasaryan.ep@gmail.com>
2023-12-02 15:04:45 +0400

    Изменена схема авторизации

--- components/backend/ems/ems/adapters/http_api/auth.py
+++ components/backend/ems/ems/adapters/http_api/auth.py
@@ -1,18 +1,22 @@
-from fastapi import HTTPException, status, Request
+from typing import Annotated
+
+from fastapi import HTTPException, status, Request, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from ems_libs.security import jwt
 
+jwt_scheme = HTTPBearer(scheme_name='JWT Bearer')
+
 
 async def get_auth_payload(
-        request: Request
+        token: Annotated[HTTPAuthorizationCredentials, Depends(jwt_scheme)]
 ) -> dict | None:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    claims = jwt.decode_token(request.cookies.get('token', ''))
+    claims = jwt.decode_token(token.credentials)
     if claims is None:
         raise credentials_exception
     return claims
-
