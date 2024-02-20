431ad5854e0acec50faea12923335ff05d4038fe
Константин Михайлин <mik-dmitry.k@yandex.ru>
2024-02-20 21:33:23 +0400

    wip: Описаны таблицы, DTO, entities для Секций

---
+++ components/backend/ems/ems/application/dto/response/club.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class ClubResponse(BaseModel):
+    id: int = Field(
+        gt=0,
+        description='Уникальный идентификатор'
+    )
+    title: str = Field(
+        description='Название'
+    )
+    description: str = Field(
+        description='Описание'
+    )
+    telegram: Optional[str] = Field(
+        default=None,
+        description='Telegram'
+    )
+    vk: Optional[str] = Field(
+        default=None,
+        description='VK'
+    )
+    youtube: Optional[str] = Field(
+        default=None,
+        description='YouTube'
+    )
+    rutube: Optional[str] = Field(
+        default=None,
+        description='Rutube'
+    )
+    tiktok: Optional[str] = Field(
+        default=None,
+        description='TikTok'
+    )
+    is_favorite: bool = Field(
+        default=False,
+    )
