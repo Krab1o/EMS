f36c7e7da9ffdac68ec156469b7151c3697227a9
Константин Михайлин <mik-dmitry.k@yandex.ru>
2024-02-24 21:03:36 +0400

    wip: Описан сервис и интерфейс репозитория для Секций, также дополненны __init__ файлы и зависимости

---
+++ components/backend/ems/ems/application/interfaces/club_repository.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from ems.application import entities, dto
+
+
+class IClubRepository(ABC):
+    @abstractmethod
+    async def get_by_id(self, id_: int) -> Optional[entities.Club]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_list(self, page: int, size: int) -> list[entities.Club]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def add_one(self, data: dto.ClubCreateRequest) -> Optional[int]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def update_one(self, data: dto.ClubUpdateRequest) -> Optional[int]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def delete_one(self, club_id: int):
+        raise NotImplementedError
+
+# мне кажетсячто надо писать update для каждого поля таблицы Сервисов
