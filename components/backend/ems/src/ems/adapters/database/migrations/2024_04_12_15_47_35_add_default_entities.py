a1b725979c3d6038fe040f4c64eaee695ba8b16a
Evgeny Mangasaryan <mangasaryan.ep@gmail.com>
2024-04-12 23:56:35 +0400

    hotfix: Fix migrations not using IDs

--- components/backend/ems/src/ems/adapters/database/migrations/2024_04_12_15_47_35_add_default_entities.py
+++ components/backend/ems/src/ems/adapters/database/migrations/2024_04_12_15_47_35_add_default_entities.py
@@ -25,7 +25,7 @@ depends_on = None
 
 institution_data = [
     {
-        "id": 1,
+        # "id": 1,
         "title": "КНиИТ",
         "description": "Факультет компьютерных наук и информационных технологий",
     }
@@ -33,7 +33,7 @@ institution_data = [
 
 user_data = [
     {
-        "id": 1,
+        # "id": 1,
         "last_name": "Иванов",
         "first_name": "Иван",
         "middle_name": "Иванович",
@@ -48,7 +48,7 @@ user_data = [
         "password": "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918",
     },
     {
-        "id": 2,
+        # "id": 2,
         "last_name": "Петров",
         "first_name": "Пётр",
         "middle_name": "Петрович",
@@ -66,27 +66,27 @@ user_data = [
 
 event_type_data = [
     {
-        "id": 1,
+        # "id": 1,
         "title": "Образовательные мероприятия",
         "description": "Семинары, лекции, мастер-классы",
     },
     {
-        "id": 2,
+        # "id": 2,
         "title": "Культурные мероприятия",
         "description": "Выставки искусства, музыкальные фестивали",
     },
     {
-        "id": 3,
+        # "id": 3,
         "title": "Профессиональные мероприятия",
         "description": "Карьерные ярмарки, семинары по карьере",
     },
     {
-        "id": 4,
+        # "id": 4,
         "title": "Спортивные мероприятия",
         "description": "Соревнования по спорту",
     },
     {
-        "id": 5,
+        # "id": 5,
         "title": "Тематические мероприятия",
         "description": "Мероприятия по актуальным темам",
     },
@@ -94,7 +94,7 @@ event_type_data = [
 
 event_data = [
     {
-        "id": 1,
+        # "id": 1,
         "title": "Хакатон",
         "description": "Мероприятие по программированию",
         "cover_id": None,
@@ -108,7 +108,7 @@ event_data = [
         "created_at": datetime.now() 
     },
     {
-        "id": 2,
+        # "id": 2,
         "title": "Студвесна",
         "description": "Встреча нового учебного года",
         "cover_id": None,
@@ -122,7 +122,7 @@ event_data = [
         "created_at": datetime.now()
     },
     {
-        "id": 3,
+        # "id": 3,
         "title": "Кинопоказ",
         "description": "Проекция фильма для студентов",
         "cover_id": None,
@@ -136,7 +136,7 @@ event_data = [
         "created_at": datetime.now()
     },
     {
-        "id": 4,
+        # "id": 4,
         "title": "Квест для первокурсников",
         "description": "Интерактивная игра для новичков",
         "cover_id": None,
@@ -150,7 +150,7 @@ event_data = [
         "created_at": datetime.now()
     },
     {
-        "id": 5,
+        # "id": 5,
         "title": "День рождения факультета",
         "description": "Празднование дня рождения факультета",
         "cover_id": None,
@@ -164,7 +164,7 @@ event_data = [
         "created_at": datetime.now()
     },
     {
-        "id": 6,
+        # "id": 6,
         "title": "Утро добрым бывает",
         "description": "Благотворительный проект",
         "cover_id": None,
@@ -178,7 +178,7 @@ event_data = [
         "created_at": datetime.now()
     },
     {
-        "id": 7,
+        # "id": 7,
         "title": "Встреча с работодателями",
         "description": "Серия встреч с представителями компаний",
         "cover_id": None,
@@ -192,7 +192,7 @@ event_data = [
         "created_at": datetime.now()
     },
     {
-        "id": 8,
+        # "id": 8,
         "title": "День открытых дверей",
         "description": "День, когда студенты могут посетить университет",
         "cover_id": None,
@@ -206,7 +206,7 @@ event_data = [
         "created_at": datetime.now()
     },
     {
-        "id": 9,
+        # "id": 9,
         "title": "Олимпиада",
         "description": "Соревнования по различным дисциплинам",
         "cover_id": None,
@@ -220,7 +220,7 @@ event_data = [
         "created_at": datetime.now()
     },
     {
-        "id": 10,
+        # "id": 10,
         "title": "Концерт",
         "description": "Музыкальное мероприятие",
         "cover_id": None,
@@ -234,7 +234,7 @@ event_data = [
         "created_at": datetime.now()
     },
     {
-        "id": 11,
+        # "id": 11,
         "title": "Репетиция",
         "description": "Подготовка к концерту",
         "cover_id": None,
@@ -251,7 +251,7 @@ event_data = [
 
 club_data = [
     {
-        "id": 1,
+        # "id": 1,
         "title": "Клуб разработки",
         "description": "Секция для студентов, интересующихся разработкой программного обеспечения и технологиями.",
         "telegram": "club_programming",
@@ -261,7 +261,7 @@ club_data = [
         "tiktok": "club_programming_tiktok"
     },
     {
-        "id": 2,
+        # "id": 2,
         "title": "Клуб робототехники",
         "description": "Секция для студентов, заинтересованных в робототехнике и робототехнических проектах.",
         "telegram": "club_robotics",
@@ -271,7 +271,7 @@ club_data = [
         "tiktok": "club_robotics_tiktok"
     },
     {
-        "id": 3,
+        # "id": 3,
         "title": "Клуб искусства и дизайна",
         "description": "Секция для студентов, стремящихся к творчеству в области искусства и дизайна.",
         "telegram": "club_art_design",
@@ -281,7 +281,7 @@ club_data = [
         "tiktok": "club_art_design_tiktok"
     },
     {
-        "id": 4,
+        # "id": 4,
         "title": "Клуб науки и исследований",
         "description": "Секция для студентов, желающих участвовать в научных исследованиях и проектах.",
         "telegram": "club_science_research",
@@ -291,7 +291,7 @@ club_data = [
         "tiktok": "club_science_research_tiktok"
     },
     {
-        "id": 5,
+        # "id": 5,
         "title": "Клуб экологии и устойчивого развития",
         "description": "Секция для студентов, заинтересованных в экологических вопросах и устойчивом развитии.",
         "telegram": "club_ecology",
