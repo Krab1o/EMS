20280fc9ad2c6a3ec8e42e8b30fb8edd7c3b4447
Evgeny <mangasaryan.ep@gmail.com>
2024-04-17 22:01:30 +0400

    ci/cd: Improved development experience by allowing hot-reload in the dev version of backend

--- components/backend/ems/entrypoint.sh
+++ components/backend/ems/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python -m ems.composites.alembic_runner upgrade head
-uvicorn --app-dir src ems.composites.http_api:app --host 0.0.0.0 --port 3000
+uvicorn --app-dir src ems.composites.http_api:app --host 0.0.0.0 --port 3000 "$@"
